79915e4561264c7bfae00de2edc9e439eb2f8171
unknown <manish.aec1104@gmail.com>
2018-02-09 17:28:00 +0530

    testing 2

--- Eclipse_Workspace/BasicJava/src/com/mkp/basic/Program_0.java
+++
@@ -1,9 +0,0 @@
-
-package com.mkp.basic;
-
-public class Program_0 {
-public static void main(String[] args) {
-	System.out.println("fjfhg");
-}
-}
-
--- Eclipse_Workspace/BasicJava/src/com/mkp/basic/Program_1.java
+++
@@ -1,9 +0,0 @@
-
-package com.mkp.basic;
-
-public class Program_1 {
-	public static void main(String[] args) {
-		System.out.println("fjfhg");
-	}
-}
-
