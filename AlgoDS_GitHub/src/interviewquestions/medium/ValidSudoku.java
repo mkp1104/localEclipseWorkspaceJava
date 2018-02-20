9b7d2e80ceeb6a4527d960a125a60c50ef7da55b
mkp1104 <manish.aec1104@gmail.com>
2018-02-21 00:29:04 +0530

    21-02-2018

---
+++ AlgoDS_GitHub/src/interviewquestions/medium/ValidSudoku.java
@@ -0,0 +1,7 @@
+'ªàwñêqÒÅRÚ$=ÏÃÕá·ôï–ÚÄŒyœ<–euHÊ´ðk¯C¤¸w©šÆ¯ðcÔ»N"ÚZ0ÒäIDoÂºÈŠô.Œïv¤ír^\ËŒ—Úá‘ÇX.„GÏ@blðNaïÈ¡<Y§Ê™ûR³…þ#Eú…»w!_©È–D@çiŸ<¤08Û™5€4aýQ@„9ë®!›r›Dþ;0‹ÏŸgq<´€#ª¸´Ï)¶öVñ‹NB
+ãŽú&Òˆ™õFVCEÍ»Qç‘|eS/¾â
+¸Þ{žÂí0	S0ûà¸î‚»®ÿ"dar0Gaûq8 ;ü0|>¿®O"ü{ ¾ p÷y¿WkÚèÖ„4ž€ä1Ÿü5-ùÊ¤O«ýF@CÊ$Ý^µDŽÀl^D!¨ñ£^Þ†ªRÉiò=¡œä7O ÿ}:JEb5¥LÏˆòzÞÕ‡tÝî¹ùâÿ,4ò’(º¹1Á8asç¡áö™{\~’dy¬ø'Ð°st|Pô??µŒLŒívÌ·P÷!¿/+[--ägÕø¿‰û’hM6àéb¹!IäÅEÜ‡rÿOPŽg\‚›ð…)ÒÏ»Ùud Û‚û´—²[ì°Qõ&Ôò®kázuÇÀÖún…w"ïÚ\}u‹°vCIm—Äèƒ÷ÂA8³p‰LtÃEÜ‰e¨Àãázd@|yå3ð!$½s0ñüü2|~>	ŸF®ø9ø<|wð—àeRwÇrÕ#¥¹}y–/Ö
+™ªä}ä«$p.ÇÙÊ^¤[pl³;`ÆÙû¡€C"º?‰ƒyRÿÃð«ð
+¾ q°\ÎžÍ³v±E³r5r±`3/ñlúÂ~Í)üŠÇqô„Î_äñ¬xÂ?uòÉÇÀÅ^L*^sêlÝ¨µ]oEÍÞ(±gÚÿ!ôyÖ¹Dâ9‹\îIœÅãXžÇMüÌk¹T¹[l´—eN#HŒãr+à&^ô_Àe'ŽbÍøj¶ÊwÒQ¦ÐŠÝu¿3…²æS¾°T‡|¯4±§q±_ŽÔ×<ñèiIHÌ£ƒ@TìÓ8u§òb)]ÇtzÁ¡pü ÷Áû]“5ïW¤Á?É<ÓâD=î¿‡%@³‰4Sí±ÉœˆÜ†Õ#åÁ¨C Ï‰»ð—ö˜	HL­Þ+QV#Oº6}^¯k•þ·1†þªÏhÊx·nãNƒ1øod‚hÄ±+¿Û‚ê˜Äm‚\Ì7Ä:b	7¥Æ¹hèŠ1ˆu"A’1l{ÈF\äœšÄ.÷ÂÞ¢"ÞxÀ/ã$ŒnV®F…˜3u²íñð¨Ÿaû{jLCµû(¯”-“ õý÷0²ôìÛð^Ò¡ˆ·BrrqQqà«÷Þ Üwu†y+a=ŽZežê2³yVÆy!ÎðAý,sÓË%£b67X |rär¼ŠÝ¥â¬ôj<DÞÕêèã\`žÙ¼é8…†!LþýlüUdH¾`—:üß›ñ– ¾·f*"p™×¤I£›åCÄêš2?+cGõÙÜ«[hD›s9±zQ›Žä
+‹cx£œÃõ|©$™ÁÍ,¼p-ÎÁüÝÛUá¯%!@ì÷ÎD1¬“Xýp>cçªÑÖ8Ü¿¹ÿÕÌeÝe¢Gpå…ýº2–è€&~åÀi\íÇ‘û¿€²H¸zÜïþlòAÑ¦ê5–ÜH-¹Wï|
+sl‹à+îCRûrÑÏ¡@ËLÌLí¥¹š‡U®óI¼ó‘!Õüºz&!ú"¢×SJ§g‘*Û¼$¾$ ‹>±k‚ñÄ} Ð{ÊÝ×25{$·Ý™5
---
+++ AlgoDS_GitHub/src/timus/A3801893.java
@@ -0,0 +1,6 @@
+.^cï^7‰n]¿ä²†‡±KÑK…—,â—,âTç; 2¤ó†L¢î®W3‘×.×ÖcÌa(úAZ7Þ1bOIh«HGžŒ¶5P´säCÝ7$+wäÖ¸Ûd¥¹J¹ýtz;µ#dØYcS¶èZ´ØÿíÕ'NÊ]÷uîÃ<ÀÆñReT”ËÞ·[œçWkÖo6zîö)Zß>¸^dfÃÇìƒ•¥bXº]ÝRSó”dÇÁÔ¢ÍvÓÑû²·¤Š•‘Pk/®n)T¤ìáwPK„ÐŠËÚ1u}çáø~¨R/óD
+5Y{):¤+ÓÔwÂsŒ2q—Äg¦œüþ\ˆ°1Ä¼©qŽøIÊn2+½«ñ÷Ù“t&	 cßÕRŽ4Ðz³æK Oh#aôßÄ·{)æ©Eº1¼1F·»ƒâ‘Ê†PÒ$›D@\c‡Óð?55&Ìý’bê	ÃÖ‚°ïHŠ%N"—}?ì»SD™«¿y…¬ÎÈ$¯X£­‹-áS¸(—Ë™T\[qÚEÖ½ÿ¤Ä°ŸJÿ+¦J\¨E|>ŠÕÄ¶Õ†$Aªvì‡UùòI€Èƒ÷gpLð–å^`éÂ0ø²á}ýŠôŸ<ÈF}R{7¬¥ÂX‚ìúx
+Œ<¿{ÃÇ}Í¡·GÖˆOÑ]~Óþ<TÙ	
+õ¬ÈµÜÀ@·þ®dZEX[OUò£;xŒj'«t«ÕRÛ&[ü!D™Çaóh±5œ­øÍbeÿU#¶o$„#"r[Ér^æ=`*¡"òZ¯^^KbÄKÖ$åd"áê¸.2ÃŽYô$'‹ß*’Zßh$|õ­d&èÔÙ®Ñª9VdpÙY(uï
+Cû§”¡¤°`Û%PkúÜ¸øÝi’1ä,.t9¤öOÊbä™‰Pm£§6O{~†›)OSÈò,:íÊü× Ñl­?ÎÄJ¨yrì,gºu–úzŸ«‘Ùæ/Â§tÛ•éØNyýv~¢½wÀoÝÐ–¶Cêý6u½=¢-F~$²ÏG›ÀµæZ_>ñ
+Dÿ,­¶Q:æ©fJ—ñÔ)=ÀaÉdZ|c‡²OÑo¨ÂJ¥@A@ù¦îÌÊ×G8Ó|Üÿš"ÆfšàU«c_ÎÖ•Â1Q?ÿZè0œkIæƒ0ÖV« ¨$›â—ÆÂQ#“X¬ÊãzâØ`Dî]&o°&’£xvÑÎP‚Z¶A”ÕØz‹0ÔÂ¥.ò1¤GôÊr¤ó%ólæºÁÛHÑh_6xMH@ml/Ö‘—&˜ƒ·˜®ðP;É2ý¼r/Qòåž*#$wï8¥wï,cÏ®L¹äš3Ið¨¢‘š#›hòJpÎÀ‡¢fëU$‰íUÙm
