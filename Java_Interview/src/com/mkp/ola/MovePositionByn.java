3ef67bdf04ff2233e2bcb63393fa53218692eed7
mkp1104 <manish.aec1104@gmail.com>
2018-02-25 12:51:57 +0530

    25-02-2018

---
+++ Java_Interview/src/com/mkp/ola/MovePositionByn.java
@@ -0,0 +1,18 @@
+package com.mkp.interview;
+
+import java.util.Scanner;
+
+public class MovePositionByn{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter value of n: ");
+        int n = sc.nextInt();
+        System.out.println("Enter the number: ");
+        sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        if(sc!=null){
+            sc.close();
+        }
+        
+    }
+}
